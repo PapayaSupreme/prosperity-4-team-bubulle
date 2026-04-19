404b777bcd1973046c7cd2af910419175eb3e3d8
Pablo <pablo.compte.jeux@gmail.com>
2026-04-19 16:17:23 +0200

    update manual round 2 to final version

--- prosperity4/manual/round2.py
+++ prosperity4/manual/round2.py
@@ -16,29 +16,29 @@ def scale(x: int) -> float:
     else:
         return x * 0.07
 
-def max_with_speed(x: int) -> int:
+def speed(x: int) -> float:
+    """Returns the PnL's multiplier produced by a speed investment of x% of 50K XIRECS."""
+    if x > 100:
+        return -1
+    return 0.1 + 0.008 * x
+
+def max_with_speed(x: int, details: bool) -> int:
     """returns the maximum value obtainable with x% of 50K XIRECS allocated to speed"""
     pnls = np.zeros(101)
+    speed_value = speed(x)
     # 0 -> research(0), scale(100)   100 -> research(100), scale(0)
     for y in range(101 - x):
-        pnls[y] = round(round(research(y)) * scale(100 - x - y) * 0.1)
-    if np.max(pnls) <= 70000:
-        return 0
+        pnls[y] = round(research(y) * scale(100 - x - y) * speed_value)
+    if details:
+        print("Research % for best PnL: ",  np.where(pnls == np.max(pnls)))
+        print("Speed % for best PnL: ", x)
+        print("Scale % for best PnL: 100 - Research - Speed")
     return np.max(pnls)
 
-speed_mult = 0.2
-speed_value = 4
 pnl_all = np.zeros(101)
-# 0 -> research(0), scale(100)   100 -> research(100), scale(0)
-for i in range(101 - speed_value):
-    pnl_all[i] = int(int(research(i)) * scale(100 - speed_value - i) * speed_mult)
-#plot pnl_all as y and [0, 100] as x
-#print(np.max(pnl_all), np.where(pnl_all == np.max(pnl_all)))
-"""for i in range(101):
-    pnl_all[i] = max_with_speed(i)
-plt.xlabel('Speed Investment (%)')
-plt.ylabel('Max PnL combination of Research & Scale')
-plt.title('PnL vs Speed Investment')"""
+for speed_iter in range(101):
+    pnl_all[speed_iter] = max_with_speed(speed_iter, False)
+
 x_abs = np.arange(0, 101)
 plt.plot(x_abs, pnl_all)
 plt.xlabel('Speed Investment (%)')
@@ -46,10 +46,9 @@ plt.ylabel('Max PnL combination of Research & Scale')
 plt.title('PnL vs Speed Investment')
 plt.grid()
 plt.show()
-print(np.max(pnl_all), np.where(pnl_all == np.max(pnl_all)))
 
-# 100: 23 * 77 : 74 233
-# 99: 23 * 76 : 73 269
-# 98: 23 * 75 : 72 305
-# 97: 23 * 74 : 71 341
-# 96: 22 * 74 : 70 385
+print("Best PnL: ", np.max(pnl_all),  " at index: ",  np.where(pnl_all == np.max(pnl_all)))
+#best is at index 36
+print("Combination for best Pnl: ", max_with_speed(36, True))
+# Best: 16, 48, 36 -> 160 065
+
